cad3050f6fddeb1b015b2f3d4483cd6bf1a7b0d9
claudiabdm <claudiabdm@hotmail.es>
2020-09-16 23:11:39 +0200

    Delete plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,6 @@ export default {
     ],
     script: [
       {src: "https://unpkg.com/smoothscroll-polyfill/dist/smoothscroll.min.js"},
-      {src: "https://unpkg.com/smoothscroll-anchor-polyfill"},
     ]
   },
   /*
